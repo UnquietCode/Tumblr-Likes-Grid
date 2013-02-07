604c7e2b9be2e217302a904f3c0cbe83156108ff
Benjamin Fagin <blouis@unquietcode.com>
2013-02-07 15:09:27 -0800

    fix

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 coffeescript-concat -o output.coffee scripts/UQC.coffee scripts/ContentHelper.coffee scripts/Likes.coffee
 coffee -o . output.coffee
-mv output.js content/js/likes.js
+mv output.js client/js/likes.js
 rm output.coffee
